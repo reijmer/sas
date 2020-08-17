e012ea4f97147e94a38565cc136d492c39af05d6
Bart Reijmer <bart.reijmer@roche.com>
2020-08-17 21:39:25 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,15 @@ __version__ = 0.1
 setup(
     name="sas",
     version=__version__,
+    install_requires=[
+        'click',
+        'pandas',
+        'numpy',
+        'termtables',
+        'dataframe_sql'
+    ],
+    author = 'Bart Reijmer',
+    author_email = 'bart.reijmer@roche.com',
     packages=find_packages(),
     entry_points={"console_scripts": ["sas=sas.reader:main"]},
 )
