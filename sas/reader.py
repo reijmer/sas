9829a69dcb1980fb29fd3b4dbb82615130d88c89
Bart Reijmer <bart.reijmer@roche.com>
2020-09-04 15:48:52 +0200

    forgot what i did, committing anyways. bad.

--- sas/reader.py
+++ sas/reader.py
@@ -10,185 +10,232 @@ import click
 from dataframe_sql import register_temp_table, query
 
 
-def read(fname: str) -> pd.DataFrame:
-
-    df = pd.read_sas(fname, format='sas7bdat', encoding='iso-8859-1')
-    df = df.fillna('')
-
-    df = df.astype(str)
-
-    table = fname.replace('.sas7bdat', '')
-    reg = register_temp_table(df, table)
-
-    return df, table
+class FileReader:
 
 
-def get_dimensions(data, column_index=0) -> tuple:
-    """
-    Determines how many rows and columns can be display in the 
-        terminal window at once.
+    def read(self, fname: str) -> pd.DataFrame:
 
-    """
+        if not os.path.isfile(fname):
+            print('File not found.')
+            return None, None
 
-    ROW_SIZE = 3 # every row requires 3 lines.
-    COL_SIZE = 4 # every columns has 4 chars in formatting.
+        df = pd.read_sas(fname, format='sas7bdat', encoding='iso-8859-1')
+        df = df.fillna('')
+
+        df = df.astype(str)
+
+        table = fname.replace('.sas7bdat', '')
+        reg = register_temp_table(df, table)
+
+        return df, table
+
+
+class Display:
+
+
+    def __init__(self):
+        self.COLUMN_INDEX = 0
+        self.PAGE_INDEX = 0
+
+        self.clear_screen()
+
+
+    def get_dimensions_y(self) -> tuple:
+        """
+        Determines how many rows and columns can be display in the 
+            terminal window at once.
+
+        """
+
+        ROW_SIZE = 2.22 # every row requires 3 lines.
+        
+        _, height = shutil.get_terminal_size((80, 20))
+        
+        rows = math.floor(height / ROW_SIZE)
+        rows = int(rows)
+
+        return rows
+
+    def get_dimensions_x(self, data: pd.DataFrame, column_index=0) -> tuple:
+        """
+        Determines how many rows and columns can be display in the 
+            terminal window at once.
+
+        """
+
+        COL_SIZE = 4 # every columns has 4 chars in formatting.
+        
+        width, _ = shutil.get_terminal_size((80, 20))
+        
+        column_sizes = self.get_max_column_sizes(data, COL_SIZE)
+        column_sizes = column_sizes[column_index:]
+
+        col_length = 0
+        num_cols = 0
+
+        while col_length < width and num_cols < len(column_sizes):
+            if col_length + column_sizes[num_cols] <= width:
+                col_length += column_sizes[num_cols]
+                num_cols += 1
+            else:
+                break
+
+
+        return num_cols
+
+    def get_max_column_sizes(self, df, col_size=0) -> list:
+        """
+        Calculates the maximum length of each column in a dataframe.
+        """
+
+        measurer = np.vectorize(len)
+        lengths = measurer(df.values.astype(str)).max(axis=0)
+
+        for i, col in enumerate(list(df.columns)):
+            if len(col) > lengths[i]:
+                lengths[i] = len(col)
+
+        lengths += col_size
+
+        return lengths
     
+
+    def display_page(self, data, clear=True) -> None:
+        
+        if clear:
+            self.clear_screen()
+
+        tt.print(
+            data.values.tolist(),
+            style=tt.styles.thin,
+            header=list(data.columns))
+
+
+    def char_input(self) -> str:
+
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+        return ch
+
+
+    def clear_screen(self) -> None:
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+
+    def print_options(
+            self,
+            rows,
+            columns,
+            data
+        ) -> None:
+
+        formatting = '\x1b[0;30;47m{}\x1b[0m'
+
+        num_cols = len(list(data.columns))
+        num_rows = len(data)
+
+        reset_option = '\t' if not self.queried else '| r: reset'
+
+        output = 'rows {}-{} ({}) | cols {}-{} ({})  \t ↑↓←→ to scroll \t q: quit | f: sql {}\t' \
+                    .format(
+                        (self.PAGE_INDEX*rows)+1,
+                        (self.PAGE_INDEX*rows+rows if self.PAGE_INDEX*rows+rows < num_rows else num_rows),
+                        num_rows,
+                        (self.COLUMN_INDEX)+1,
+                        (self.COLUMN_INDEX+columns if self.COLUMN_INDEX+columns < num_cols else num_cols),
+                        num_cols,
+                        reset_option)
+
+        print(formatting.format(output))
+
     
-    width, height = shutil.get_terminal_size((80, 20))
-    
-    column_sizes = get_max_column_sizes(data, COL_SIZE)
-    column_sizes = column_sizes[column_index:]
+    def handle_input(self):
 
-    rows = math.floor(height / ROW_SIZE)
-    rows = int(rows)
+        key = self.char_input()
 
-    col_length = 0
-    num_cols = 0
+        if key == 'q':
+            return False
 
-    while col_length < width and num_cols < len(column_sizes):
-        if col_length + column_sizes[num_cols] <= width:
-            col_length += column_sizes[num_cols]
-            num_cols += 1
-        else:
-            break
+        elif key == 'B':
+            if (self.PAGE_INDEX + 1) * self.rows < len(self.data):
+                self.PAGE_INDEX += 1
 
+        elif key == 'A':
+            if self.PAGE_INDEX > 0:
+                self.PAGE_INDEX -= 1
 
-    return rows, num_cols
+        elif key == 'D':
+            if self.COLUMN_INDEX > 0:
+                self.COLUMN_INDEX -= 1
 
+        elif key == 'C':
+            if self.COLUMN_INDEX+self.columns < len(list(self.data.columns)):
+                self.COLUMN_INDEX += 1
+            
 
-def get_max_column_sizes(df, col_size=0) -> list:
-    """
-    Calculates the maximum length of each column in a dataframe.
-    """
+        elif key == 'r':
+            self.data = query(f'select * from {self.table_name}')
+            self.queried = False
 
-    measurer = np.vectorize(len)
-    lengths = measurer(df.values.astype(str)).max(axis=0)
+            self.COLUMN_INDEX = 0
+            self.PAGE_INDEX = 0
 
-    for i, col in enumerate(list(df.columns)):
-        if len(col) > lengths[i]:
-            lengths[i] = len(col)
+        elif key == 'f':
+            sql_query= input('sql: ')
+            if sql_query is not None and sql_query.strip() != '':
+                self.data = query(sql_query)
+                self.queried = True
 
-    lengths += col_size
+                self.COLUMN_INDEX = 0
+                self.PAGE_INDEX = 0
 
-    return lengths
-    
-
-def display_page(data, clear=True) -> None:
-    
-    if clear:
-        clear_screen()
-
-    tt.print(
-        data.values.tolist(),
-        header=list(data.columns))
+        return True
 
 
-def char_input() -> str:
+    def render(self, table_name: str, data: pd.DataFrame):
 
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
+        self.clear_screen()
 
-    try:
-        tty.setraw(fd)
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        self.data = data
+        self.table_name = table_name
 
-    return ch
+        self.queried = False
 
+        while True:
+        
+            self.rows = self.get_dimensions_y()
+            
+            df = self.data[(self.PAGE_INDEX * self.rows):].head(self.rows)
 
-def clear_screen() -> None:
-    os.system('cls' if os.name == 'nt' else 'clear')
+            self.columns = self.get_dimensions_x(df, column_index=self.COLUMN_INDEX)
 
+            df = df.iloc[:, self.COLUMN_INDEX:(self.COLUMN_INDEX + self.columns)]
 
-def print_options(
-        page_index,
-        column_index,
-        rows,
-        columns,
-        data,
-        queried
-    ) -> None:
+            self.display_page(df)
+            self.print_options(self.rows, self.columns, self.data)
 
-    formatting = '\x1b[0;30;47m{}\x1b[0m'
-
-    num_cols = len(list(data.columns))
-    num_rows = len(data)
-
-    reset_option = '\t' if not queried else '| r: reset'
-
-    output = 'rows {}-{} ({}) | cols {}-{} ({})  \t ↑↓←→ to scroll \t q: quit | f: sql {}\t' \
-                .format(
-                    (page_index*rows)+1,
-                    (page_index*rows+rows+1 if page_index*rows+rows+1 < num_rows else num_rows),
-                    num_rows,
-                    (column_index)+1,
-                    (column_index+columns+1 if column_index+columns+1 < num_cols else num_cols),
-                    num_cols,
-                    reset_option)
-
-    print(formatting.format(output))
+            hi = self.handle_input()
+            if not hi:
+                break
 
 
 @click.command()
 @click.option('--fname', '--f', required=True, help='input file')
 def main(fname):
-    clear_screen()
-
-    print('Loading')
-
-    data, table = read(fname)
     
-    PAGE_INDEX = 0
-    COLUMN_INDEX = 0
+    print('Loading')
+    reader = FileReader()
+    data, table = reader.read(fname)
 
-    queried = False
+    if data is not None:
+        display = Display()
+        display.render(table, data)
 
-    while True:
-        
-        rows, columns = get_dimensions(data, column_index=COLUMN_INDEX)
-        
-        df = data[(PAGE_INDEX * rows):].head(rows)
-
-        rows, columns = get_dimensions(df, column_index=COLUMN_INDEX)
-
-        df = df.iloc[:, COLUMN_INDEX:(COLUMN_INDEX + columns)]
-
-        display_page(df)
-        print_options(PAGE_INDEX, COLUMN_INDEX, rows, columns, data, queried)
-
-        
-        key = char_input()
-
-        if key == 'q':
-            break
-
-        elif key == 'B':
-            if (PAGE_INDEX + 1) * rows < len(data):
-                PAGE_INDEX += 1
-
-        elif key == 'A':
-            if PAGE_INDEX > 0:
-                PAGE_INDEX -= 1
-
-        elif key == 'D':
-            if COLUMN_INDEX > 0:
-                COLUMN_INDEX -= 1
-
-        elif key == 'C':
-            if COLUMN_INDEX+columns < len(list(data.columns)):
-                COLUMN_INDEX += 1
-            
-
-        elif key == 'r':
-            data = query(f'select * from {table}')
-            queried = False
-
-        elif key == 'f':
-            sql_query= input('sql: ')
-            if sql_query is not None and sql_query.strip() != '':
-                data = query(sql_query)
-                queried = True
-
-        
         
